401b54703560cab1042c0f90f12c0ec121af8a66
mannycolon <colonmanuel7@gmail.com>
2017-03-06 18:50:17 -0500

    fixed bug with textarea html element that was using autofocus instead of autoFocus

--- View.js
+++ View.js
@@ -14,7 +14,7 @@ class View extends React.Component {
         <div style={style.paper}>
           <div style={style.sideline}></div>
             <div style={style.paperContent}>
-              <textarea autofocus style={style.textarea} placeholder="Notes"
+              <textarea autoFocus style={style.textarea} placeholder="Notes"
                         value={this.props.comment}
                         onChange={this.props.handleComment.bind(this)}
                         onBlur={this.props.handleSaveComment.bind(this)}/>
